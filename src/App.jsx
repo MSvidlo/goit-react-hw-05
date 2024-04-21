522a42e4565201dff5f3b8a6c389377acfa36488
MSvidlo <143841475+MSvidlo@users.noreply.github.com>
2024-04-21 21:52:05 +0200

    adding folders: Pages and Component
    changing file App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,33 @@
-
-import { Routes, Route } from "react-router-dom";
-import HomePage "path/to/pages/Home";
-import About from "path/to/pages/About";
-import Products from "path/to/pages/Products";
+import { Routes, Route, NavLink } from 'react-router-dom';
+import HomePage from "./pages/HomePage";
+import MovieDetailsPage from "./pages/MovieDetailsPage/MovieDetailsPage";
+import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
+import MoviesPage from "./pages/MoviesPage/MoviesPage";
+import MovieCast from './components/MovieCast/MovieCast';
+import MovieReviews from './components/MovieReviews';
+import css from './App.module.css'
 
 export const App = () => {
   return (
-    <div>
+    <div className={css.container}>
+      <header className={css.header}>
+        <nav className={css.nav}>
+           <Link to="/" className={css.homePage}>
+            Home
+          </Link>
+          <Link to="/movies" className={css.movie}>
+            Movies
+          </Link>
+        </nav>
+</header>
+    
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/products" element={<Products />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/movies" element={<MoviesPage />} />
+        <Route path="/movies/:movieId" element={<MovieDetailsPage />} />
+        <Route path="/movies/:movieId/cast" element={<MovieCast />} />
+        <Route path="/movies/:movieId/reviews" element={<MovieReviews />} />  
+        <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </div>
   );
---
+++ src/pages/MoviesPage/MoviesPage.jsx
@@ -0,0 +1,35 @@
+// import { Field, Form, Formik } from 'formik';
+// import toast, { Toaster } from 'react-hot-toast';
+
+
+const MoviesPage = () => {
+
+    //     const notify = () => toast('Please type a desired word.');
+    //      function handleSubmit(values, actions) {
+    //     if (values.searchedText.trim() !== '') {
+    //       onFormSubmit(values.searchedText);
+    //       actions.resetForm();
+    //     } else {
+    //       notify();
+    //     }
+    //   }
+    // return (<div>
+    // <Formik initialValues={{ searchedText: '' }} onSubmit={handleSubmit}>
+    //         <Form className={css.form}>
+    //           <div className={css.fieldWrapper}>
+           
+    //             <Field
+    //               className={css.input}
+    //               name="searchedText"
+    //               type="text"
+    //               autoComplete="off"
+    //               autoFocus
+    //               placeholder="Search images and photos"
+    //                       />
+    //                        <button className={css.button} type="submit">Search
+    //             </button>
+    //           </div>
+    //         </Form>
+    //       </Formik>
+    //     <Toaster position="top.right" /></div>)
+}
