baf446c7144ae6ecc105d71f111573c9ab673b4d
MSvidlo <143841475+MSvidlo@users.noreply.github.com>
2024-04-24 23:38:36 +0200

    adding movieDatailsPage

--- src/pages/MovieDetailsPage/MovieDetailsPage.jsx
+++ src/pages/MovieDetailsPage/MovieDetailsPage.jsx
@@ -1,8 +1,48 @@
+import { fetchMovieDetails } from '../../Js/api';
+import {Link, Outlet, useLocation, useParams } from 'react-router-dom';
+import { useRef } from 'react';
+import ErrorMessage from '../../components/ErrorMassage/ErrorMassage';
+import LoadMoreBtn from '../../components/LoadMoreBtn/LoadMoreBtn';
+import Loader from '../../components/Loader/Loader';
+import { useEffect,useState } from 'react';
+import MovieDetails from '../../components/MovieDetails/MovieDetails'
+import GoBackBtn from '../../components/GoBackBtn/GoBackBtn.jsx';
+import css from './MovieDetailsPage.module.css';
 
+function MovieDetailsPage() {
+    const [isFetching, setIsFetching] = useState(false);
+    const [error, setError] = useState(null);
+    const [details, setDetails] = useState(null);
+    const location = useLocation();  
+   
+ const backLinkHref = location.state ?? "/movies";
+    const { movieId } = useParams();
+    useEffect(() => {
+        const fetchData = async () => {
+             setIsFetching(true);
+    try {
+        const data = await fetchMovieDetails(movieId);
+          setDetails(data);
+      } catch (err) {
+        setError(err);
+      } finally {
+        setIsFetching(false);
+      }
+    };
 
-const MovieDetailsPage = () =>
-{
+     
+            fetchData();
+   
+    }, [movieId]);
 
+      return <div>
+    {isFetching && <Loader />}
+    {error && <ErrorMessage />}
+      <button className={css.goBackBtn}><Link to={backLinkHref}>Go Back</Link></button>
+    <MovieDetails movieData={details} />
+    <Outlet />
+  </div>;
 }
+
 
 export default MovieDetailsPage;
